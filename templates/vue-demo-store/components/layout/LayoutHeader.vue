e22dc43dd1b61f364015ec48b0b8fb79976d9bc9
ceptar <christoph.cerjan@gmail.com>
2024-02-08 08:23:59 +0100

    quick fix

--- templates/vue-demo-store/components/layout/LayoutHeader.vue
+++ templates/vue-demo-store/components/layout/LayoutHeader.vue
@@ -9,26 +9,24 @@ const sidebarController = useModal();
 
 <template>
   <header class="relative bg-white" aria-label="top-navigation">
+
+
+    <div class="py-20 mx-auto max-w-7xl px-6"> <div class="grid lg:grid-cols-4 sm:grid-cols-2 gap-8"> <div> <p class="mt-3 max-w-md mx-auto text-base sm:text-lg md:mt-5 md:text-xl md:max-w-3xl text-gray-700" selected=""> Ullamcorper dignissim cras tincidunt lobortis. Magna etiam tempor orci eu lobortis.</p> </div> <div> <p class="mt-3 max-w-md mx-auto text-base sm:text-lg md:mt-5 md:text-xl md:max-w-3xl text-gray-700"> Ullamcorper dignissim cras tincidunt lobortis. Magna etiam tempor orci eu lobortis.</p> </div> <div> <p class="mt-3 max-w-md mx-auto text-base sm:text-lg md:mt-5 md:text-xl md:max-w-3xl text-gray-700"> Ullamcorper dignissim cras tincidunt lobortis. Magna etiam tempor orci eu lobortis.</p> </div> <div> <p class="mt-3 max-w-md mx-auto text-base sm:text-lg md:mt-5 md:text-xl md:max-w-3xl text-gray-700"> Ullamcorper dignissim cras tincidunt lobortis. Magna etiam tempor orci eu lobortis.</p> </div> </div> </div>
+
+
+
+
+
     <div class="mx-auto px-4 sm:px-6">
-      <div
-        class="flex items-center border-b-2 border-secondary-100 py-6 space-x-4"
-      >
+      <div class="flex items-center border-b-1 border-secondary-100 py-6 space-x-4">
         <div class="flex justify-start items-center min-w-10 lg:min-w-12">
-          <div class="order-2 lg:order-1 ml-4 lg:ml-0">
+          <div class="ml-4 lg:ml-0">
             <NuxtLink :to="formatLink(`/`)">
               <span class="sr-only">Shopware</span>
-              <img
-                class="h-10 w-10 lg:h-12 lg:w-12"
-                src="/logo.svg"
-                alt="logo of the shop"
-                width="40px"
-                height="40px"
-              />
+              <img class="h-10 w-10 lg:h-12 lg:w-12" src="/logo.svg" alt="logo of the shop" width="40px" height="40px" />
             </NuxtLink>
           </div>
-          <div class="order-1 lg:order-2 flex justify-start items-center">
-            <LayoutSideMenu />
-          </div>
+          
         </div>
 
         <LayoutTopNavigation />
@@ -41,48 +39,34 @@ const sidebarController = useModal();
         <div class="flex items-center justify-end">
           <AccountMenu />
           <div class="flex ml-4 flow-root lg:ml-6">
-            <NuxtLink
-              class="group -m-2 p-2 flex items-center relative text-center"
-              aria-label="wishlist"
-              data-testid="wishlist-button"
-              :to="formatLink(`/wishlist`)"
-            >
+            <NuxtLink class="group -m-2 p-2 flex items-center relative text-center" aria-label="wishlist"
+              data-testid="wishlist-button" :to="formatLink(`/wishlist`)">
               <div
-                class="w-7 h-7 i-carbon-favorite text-secondary-600 hover:text-primary hover:animate-count-infinite hover:animate-heart-beat"
-              />
-              <span
-                v-if="wishlistCount > 0"
-                class="text-3 font-sm text-white absolute bg-red-600 rounded-full min-w-5 min-h-5 top-0 right-0 leading-5"
-              >
+                class="w-7 h-7 i-carbon-favorite text-secondary-600 hover:text-primary hover:animate-count-infinite hover:animate-heart-beat" />
+              <span v-if="wishlistCount > 0"
+                class="text-3 font-sm text-white absolute bg-red-600 rounded-full min-w-5 min-h-5 top-0 right-0 leading-5">
                 {{ wishlistCount }}
               </span>
             </NuxtLink>
           </div>
           <!-- Cart -->
           <div class="flex ml-4 flow-root lg:ml-6">
-            <button
-              class="group bg-transparent -m-2 p-2 flex items-center relative"
-              aria-label="Mini cart"
-              data-testid="cart-button"
-              @click="sidebarController.open"
-            >
+            <button class="group bg-transparent -m-2 p-2 flex items-center relative" aria-label="Mini cart"
+              data-testid="cart-button" @click="sidebarController.open">
               <!-- Heroicon name: outline/shopping-bag -->
-              <div
-                class="w-7 h-7 i-carbon-shopping-bag text-secondary-600 hover:text-primary"
-              />
-              <span
-                v-if="count > 0"
-                class="text-3 font-sm text-white absolute bg-primary-600 rounded-full min-w-5 min-h-5 top-0 right-0 leading-5"
-              >
+              <div class="w-7 h-7 i-carbon-shopping-bag text-secondary-600 hover:text-primary" />
+              <span v-if="count > 0"
+                class="text-3 font-sm text-white absolute bg-primary-600 rounded-full min-w-5 min-h-5 top-0 right-0 leading-5">
                 {{ count || "" }}
               </span>
-              <span class="sr-only"
-                >{{ $t("cart.itemsInCart") }}, {{ $t("cart.viewCart") }}</span
-              >
+              <span class="sr-only">{{ $t("cart.itemsInCart") }}, {{ $t("cart.viewCart") }}</span>
             </button>
           </div>
           <CheckoutSideCart :controller="sidebarController" />
         </div>
+        <div class="flex justify-start items-center">
+            <LayoutSideMenu />
+          </div>
       </div>
     </div>
   </header>
